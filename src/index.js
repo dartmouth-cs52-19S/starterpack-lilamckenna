1dd84c7d83f853821b16dc122981b8bc0e9d4907
lilamckenna <lila.20@dartmouth.edu>
2019-04-11 16:53:43 -0400

    deployyyyyed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let seconds = 0;
+
+function time() {
+  seconds += 1;
+  $('#main').html(`${seconds} seconds have passed.`);
+}
+
+setInterval(time, 1000);

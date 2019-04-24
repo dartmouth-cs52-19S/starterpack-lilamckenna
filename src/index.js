38bee00019060690e47d4902f3adbc3229e014e3
lilamckenna <lila.20@dartmouth.edu>
2019-04-23 23:03:09 -0400

    things were working but alas they are not now. fixing but almost done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import $ from 'jquery';
 import './style.scss';
-import React from 'react';
+import { React } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Switch, NavLink,
+} from 'react-router-dom';
 
 const About = (props) => {
   return <div> All there is to know about me </div>;
@@ -11,38 +12,44 @@ const Welcome = (props) => {
   return <div>Welcome</div>;
 };
 
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
 const App = (props) => {
   return (
     <Router>
       <div>
-        <Nav />
-        <Route exact path="/" component={Welcome} />
-        <Route path="/about" component={About} />
+        <Nav>
+          <li><NavLink to="/test/id1">test id1</NavLink></li>
+          <li><NavLink to="/test/id2">test id2</NavLink></li>
+        </Nav>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+
       </div>
     </Router>
   );
 };
 
+
 const Nav = (props) => {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about" exact>About</NavLink></li>
       </ul>
     </nav>
   );
 };
-
-
-let seconds = 0;
-
-function time() {
-  seconds += 1;
-  $('#main').html(`${seconds} seconds have passed.`);
-}
-
-setInterval(time, 1000);
-
 
 ReactDOM.render(<App />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,16 @@ const env = process.env.NODE_ENV || 'development';
 // set to 'production' or 'development' in your env
 
 const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
-const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  devServer: {
-    hot: true,
-  },
   mode: env,
+  output: { publicPath: '/' },
   entry: ['babel-polyfill', './src'], // this is where our app lives
   devtool: 'source-map', // this enables debugging with source in chrome devtools
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
@@ -35,13 +36,6 @@ module.exports = {
             },
           },
           {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [autoprefixer()],
-              sourceMap: true,
-            },
-          },
-          {
             loader: 'sass-loader',
             options: {
               sourceMap: true,
@@ -49,19 +43,6 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              useRelativePath: true,
-              name: '[name].[ext]',
-            },
-          },
-        ],
-      },
-
     ],
   },
   plugins: [
@@ -69,6 +50,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './200.html',
     }),
   ],
 };

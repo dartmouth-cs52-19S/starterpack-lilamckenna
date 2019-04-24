bba2d2474e0992c59b9d8fec5099c96aceadc921
lilamckenna <lila.20@dartmouth.edu>
2019-04-24 19:54:03 -0400

    fixed error wahhhooo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+/* eslint-disable */
+
 import './style.scss';
-import { React } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router, Route, Switch, NavLink,
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  NavLink,
 } from 'react-router-dom';
 
 const About = (props) => {
@@ -20,26 +25,6 @@ const FallBack = (props) => {
   return <div>URL Not Found</div>;
 };
 
-const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Nav>
-          <li><NavLink to="/test/id1">test id1</NavLink></li>
-          <li><NavLink to="/test/id2">test id2</NavLink></li>
-        </Nav>
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/test/:id" component={Test} />
-          <Route component={FallBack} />
-        </Switch>
-
-      </div>
-    </Router>
-  );
-};
-
 
 const Nav = (props) => {
   return (
@@ -47,8 +32,26 @@ const Nav = (props) => {
       <ul>
         <li><NavLink to="/" exact>Home</NavLink></li>
         <li><NavLink to="/about" exact>About</NavLink></li>
+        <li><NavLink to="/test/id1" exact>test id1</NavLink></li>
+        <li><NavLink to="/test/id2" exact>test id2</NavLink></li>
       </ul>
     </nav>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
